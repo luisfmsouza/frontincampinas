2ac7bb012e2101e37bc4b5e60ba8625c913ed0bb
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-01-31 01:07:59 -0200

    landing mais bem feitinha

---
+++ js/fonts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+WebFont.load({
+    google: {
+        families: [
+            'Open+Sans:400,700,700italic,400italic',
+            'Roboto:400,400italic,700,700italic',
+        ]
+    }
+});
